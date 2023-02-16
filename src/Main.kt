ccd50b28936ea4bff45dc0bf5b2df58d85ee9b48
Hakimkhon <hakimkhon94@gmail.com>
2023-02-16 09:45:08 +0500

    main

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,32 @@
+import data.OperatorData
+
 fun main() {
-    println("hello world")
+    var isExit = true
+    val user = User()
+    user.loadUser()
+    while (isExit) {
+        println("""
+            1 - Abonentlar ro'yxati
+            2 - Abonent qo'shish
+            3 - Abonent o'chirish
+            4 - Abonent qidirish
+            5 - Chiqish
+        """.trimIndent())
+        print("Tanlang: ")
+
+        when(readln().toInt()) {
+            1 -> user.sortingType()
+            2 -> user.addUser()
+            3 -> user.deleteUser()
+            4 -> user.findUser()
+            5 -> {
+                isExit = false
+                print("Hayr salomat bo'ling")
+            }
+            else -> {
+                print("")
+            }
+        }
+    }
+
 }
