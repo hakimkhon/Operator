476bf69c62b244ba46287333f5597d81feda04c2
Hakimkhon <hakimkhon94@gmail.com>
2023-02-16 09:13:11 +0500

    load

--- src/User.kt
+++ src/User.kt
@@ -58,4 +58,19 @@ class User : UserInterface, SortInterface {
 
     }
 
+    fun printInfo(user: OperatorData){
+        print("${user.fullName} -> ${user.phoneNumber} ${user.balance} so'm | ${user.company}")
+    }
+
+    fun loadUser(){
+        userList.add(OperatorData("Nozim Umarov", "+998907895201", 13540, "BEELINE"))
+        userList.add(OperatorData("Shuxrat Jabborov", "+998940101078", 30050, "UCELL"))
+        userList.add(OperatorData("Umida Latipova", "+998977890061", 45012, "MOBIUZ"))
+        userList.add(OperatorData("Sobir Muminov", "+998953006565", 8040, "UZMOBILE"))
+        userList.add(OperatorData("Ma'ruf Qosimov", "+998996501234", 500, "UZMOBILE"))
+        userList.add(OperatorData("Anvar Jalolov", "+998935201010", 23011, "UCELL"))
+        userList.add(OperatorData("Bobut Toshev", "+998881004700", 9481, "MOBIUZ"))
+        userList.add(OperatorData("Lola Rustamova", "+998918952019", 45610, "BEELINE"))
+    }
+
 }
